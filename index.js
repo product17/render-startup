282ea4b9f0e3f71ea822aa9650d8f35bc81d94f4
Brent Crocker <crocker.brent@gmail.com>
2015-03-27 19:32:07 -0700

    adding jade

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+,   app = express()
+,   path = require('path');
+
+
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jade');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
